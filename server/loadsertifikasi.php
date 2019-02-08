a52fa0bf48e7a89315b398bda2c6644932033d86
Ainal Munzir <ainal.munzir@gmail.com>
2019-02-08 16:58:27 +0800

    Add files via upload

---
+++ server/loadsertifikasi.php
@@ -0,0 +1,32 @@
+<?php 
+	$query1 = "SELECT * FROM sertifikasi";
+	$result1 = mysqli_query($db, $query1);
+	if (!$result1) {
+		printf("Error: %s\n", mysqli_error($db));
+		exit();
+	}
+
+	$halaman = 15;
+	$page = isset($_GET['halaman'])? (int)$_GET["halaman"]:1;
+	$mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
+	$query2 = mysqli_query($db, "SELECT * FROM sertifikasi LIMIT $mulai, $halaman");
+	$sql = mysqli_query($db, $query1);
+	$total = mysqli_num_rows($sql);
+	$pages = ceil($total/$halaman);
+	$no = $mulai+1;
+
+	if (isset($_GET['search'])) {
+		$halaman = 15;
+		$page = isset($_GET['halaman'])? (int)$_GET["halaman"]:1;
+		$mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
+		$query2 = mysqli_query($db, "SELECT * FROM sertifikasi WHERE nama LIKE '%$searchquery%' LIMIT $mulai, $halaman");
+		$sql = mysqli_query($db, $query1);
+		$total = mysqli_num_rows($sql);
+		$pages = ceil($total/$halaman);
+		$no = $mulai+1;
+	}
+
+
+
+
+ ?>
