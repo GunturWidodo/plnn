aaed5d3e32d775dc609bd6de7930f9ddff6cdce1
Ainal Munzir <ainal.munzir@gmail.com>
2019-02-13 08:38:20 +0800

    Update server.php

--- server/server.php
+++ server/server.php
@@ -1,5 +1,5 @@
 <?php 
-	session_start();
+	#session_start();
 
 	//membuat koneksi ke database
 	$db = mysqli_connect('localhost', 'root', '', 'registration');
@@ -20,5 +20,29 @@
 		mysqli_query($db, $sql);
 
 		header('location: login.php');	
+	}
+
+	if (isset($_POST['simpan'])) {
+		$nip = mysqli_real_escape_string($db, $_POST['nip']);
+		$nama = mysqli_real_escape_string($db, $_POST['nama']);
+		$jabatan = mysqli_real_escape_string($db, $_POST['jabatan']);
+		$unit = mysqli_real_escape_string($db, $_POST['unit']);
+		$kode = mysqli_real_escape_string($db, $_POST['kode']);
+		$judul = mysqli_real_escape_string($db, $_POST['judul']);
+		$pelaksana = mysqli_real_escape_string($db, $_POST['pelaksana']);
+		$nomor = mysqli_real_escape_string($db, $_POST['nomor']);
+		$dari = mysqli_real_escape_string($db, $_POST['dari']);
+		$sampai = mysqli_real_escape_string($db, $_POST['sampai']);
+		$ket = mysqli_real_escape_string($db, $_POST['ket']);
+		
+		$sql = "INSERT INTO sertifikasi (nip, nama, jabatan, unit, kode_sertifikasi, judul_sertifikasi, pelaksana, no_sertifikasi, masa_berlaku, sampai_dengan, keterangan) VALUES ('$nip', '$nama', '$jabatan', '$unit', '$kode', '$judul', '$pelaksana', '$nomor', '$dari', '$sampai', '$ket')";
+		mysqli_query($db, $sql);
+		$tesss = settype($sql, "string");
+
+		echo '<script language="javascript">';
+		echo 'alert("'.$nip.'dan'.$tesss.'")';
+		echo '</script>';
+
+		#header('location: ./sertifikasi.php');	
 	}
   ?>
