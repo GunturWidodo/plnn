c290f751d25ae4216d319135adf60899a4da4e93
Ainal Munzir <ainal.munzir@gmail.com>
2019-02-13 08:41:04 +0800

    Update loadsertifikasi.php

--- server/loadsertifikasi.php
+++ server/loadsertifikasi.php
@@ -13,19 +13,14 @@
 	$sql = mysqli_query($db, $query1);
 	$total = mysqli_num_rows($sql);
 	$pages = ceil($total/$halaman);
-	$no = $mulai+1;
 
 	if (isset($_GET['search'])) {
-		$halaman = 15;
+		$query1 = "SELECT * FROM sertifikasi WHERE nama LIKE '%$searchquery%'OR keterangan LIKE '%$searchquery%'";
 		$page = isset($_GET['halaman'])? (int)$_GET["halaman"]:1;
 		$mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
 		$query2 = mysqli_query($db, "SELECT * FROM sertifikasi WHERE nama LIKE '%$searchquery%'OR keterangan LIKE '%$searchquery%' LIMIT $mulai, $halaman");
 		$sql = mysqli_query($db, $query1);
 		$total = mysqli_num_rows($sql);
 		$pages = ceil($total/$halaman);
-		if ($pages = 1) {
-			$page = 1;
-		}
-		$no = $mulai+1;
 	}
  ?>
