a9a3b5078bfb47d55face865d563b39704b3a4fd
MuhammadDhanyFahreza <dhanyfahreza@gmail.com>
2019-02-14 09:47:21 +0700

    Add files via upload

---
+++ purnabakti.php
@@ -0,0 +1,195 @@
+<?php 
+?>
+<!DOCTYPE html>
+ 
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+</head>
+<body style="height:1500px">
+
+<div class="bar">
+  <nav class="navbar navbar-expand-sm navbar-custom" style=" background-color: #008C9E" >
+    <a class="navbar-brand" href="#" style="color: #fff; font-size: 27px"><b>Login Sebagai User</b></a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #fff; font-size: 18px">Surat Rekomendasi</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #fff; font-size: 18px">Keberangkatan</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #fff; font-size: 18px">Resitusi</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #fff; font-size: 18px">Surat Udiklat</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: #fff; font-size: 18px">Status</a>
+      </li>
+    </ul>
+  </nav>
+</div>
+<h2 style="text-align: center; margin-top: 50px;"><b>Alur Tahapan</b></h2>
+    <div class="status" style="margin-left: 90px; margin-top: 60px;">
+      <div class="row" style="margin-top: 30px">
+        <div class="col-xl-3 col-sm-4 col-mb-3">
+          <div class="card bg-success" style="width: 180px; margin-left: 130px;">
+            <div class="card-body text-center">
+              <p class="card-text" style="color: white">Surat Rekomendasi</p>
+            </div>
+          </div>
+        </div>
+        <i class="fas fa-arrow-right fa-2x" style="margin-top: 19px; margin-left: -38px; color: #343a40;"></i>
+        <div class="col-2">
+          <div class="card bg-success" style="width: 200px; margin-left: px;" >
+            <div class="card-body text-center">
+              <p class="card-text" style="color: white">Jadwal Keberangkatan</p>
+            </div>
+          </div>
+        </div>
+        <i class="fas fa-arrow-right fa-2x" style="margin-top: 19px; margin-left: -23px; color: #343a40;"></i>
+        <div class="col-2">
+          <div class="card bg-success" style="width: 150px; margin-left: 5px;">
+            <div class="card-body text-center">
+              <p class="card-text" style="color: white">Restitusi</p>
+            </div>
+          </div>
+        </div>
+         <i class="fas fa-arrow-right fa-2x" style="margin-top: 19px; margin-left: -69px; color: #343a40;"></i>
+        <div class="col-2">
+          <div class="card bg-success" style="width: 150px; margin-left: 5px;">
+            <div class="card-body text-center">
+              <p class="card-text" style="color: white">Surat Udiklat</p>
+            </div>
+          </div>
+        </div>
+         <i class="fas fa-arrow-right fa-2x" style="margin-top: 19px; margin-left: -69px; color: #343a40;"></i>
+        <div class="col-2">
+          <div class="card bg-success" style="width: 150px; margin-left: 10px;">
+            <div class="card-body text-center">
+              <p class="card-text" style="color: white">Status</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div style="width: 700px;">
+  <form style="margin-left: 220px; margin-top: 80px; border: 2px solid green; border-radius:5px;">
+    <fieldset>
+      <legend style="margin-top: 5px; margin-left: 15px;">Form</legend>
+        <p>
+            <label style="margin-left: 15px">Jadwal Keberangkatan : </label>
+            <input type="date" name="tanggal" />
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Send" style="background-color: #4CAF50; color: white; margin-left: 15px;" />
+        </p>
+    </fieldset>
+  </form>
+</div>
+<br>
+<br>
+  <h3 style="margin-left: 20px">Upload Berkas</h3>
+      <hr>
+      <div class="container">
+      <div class="row">
+        <div class="col">
+          <form method="post" action="server/uploadBerkas.php" enctype="multipart/form-data">
+            <h4>Berkas 1</h4>
+            <input id="filename" type="text" class="form-control" disabled="">
+            <input id="upload" type="file" name="myfile"> <br>
+            <button class="btn btn-success" type="submit" name="save" style="margin-top: 5px;">Upload</button>
+          </form>
+        </div>
+      <hr>
+      <div class="col">
+        <h4>Berkas 2</h4> 
+        <form method="post" action="server/uploadBerkas2.php" enctype="multipart/form-data">
+          <input id="filename2" type="text" class="form-control" disabled="">
+          <input id="upload2" type="file" name="myfile"> <br>
+          <button class="btn btn-success" type="submit" name="save2" style="margin-top: 5px;">Upload</button>
+        </form>
+      </div>
+    </div>
+      <hr>
+      <div class="row">
+        <div class="col">
+        <h4>Berkas 3</h4>
+        <form method="post" action="server/uploadBerkas3.php" enctype="multipart/form-data">
+          <input id="filename3" type="text"  class="form-control" disabled="">
+          <input id="upload3" type="file" name="myfile"> <br>
+          <button class="btn btn-success" type="submit" name="save3" style="margin-top: 5px;">Upload</button>
+        </form>
+      </div>
+      <hr>
+      <div class="col">
+        <h4>CV</h4>
+        <form method="post" action="server/uploadcv.php" enctype="multipart/form-data">
+          <input id="filename4" type="text"  class="form-control" disabled="">
+          <input id="upload4" type="file" name="myfile"> <br>
+          <button class="btn btn-success" type="submit name="save4" style="margin-top: 5px; margin-bottom: 10px;">Upload</button>
+        </form>
+      </div>
+    </div>
+    </div>
+    </div>
+    <a class="btn btn-success" type="submit" name="don" href="server/donlod.php" style="margin-top: 5px; margin-bottom: 10px;">Dl</a>
+  </div>
+</div>
+</body>
+<script type="text/javascript">
+  document.getElementById('upload').onchange = uploadOnChange;
+  document.getElementById('upload2').onchange = uploadOnChange2;
+  document.getElementById('upload3').onchange = uploadOnChange3;
+  document.getElementById('upload4').onchange = uploadOnChange4;
+
+  function uploadOnChange() {
+    var filename = this.value;
+    var lastIndex = filename.lastIndexOf("\\");
+    if (lastIndex >= 0) {
+      filename = filename.substring(lastIndex + 1);
+    }
+    document.getElementById('filename').value = filename;
+  }
+
+  function uploadOnChange2() {
+    var filename2 = this.value;
+    var lastIndex = filename2.lastIndexOf("\\");
+    if (lastIndex >= 0) {
+      filename2 = filename2.substring(lastIndex + 1);
+    }
+    document.getElementById('filename2').value = filename2;
+  }
+
+  function uploadOnChange3() {
+    var filename3 = this.value;
+    var lastIndex = filename3.lastIndexOf("\\");
+    if (lastIndex >= 0) {
+      filename3 = filename3.substring(lastIndex + 1);
+    }
+    document.getElementById('filename3').value = filename3;
+  }
+
+  function uploadOnChange4() {
+    var filename4 = this.value;
+    var lastIndex = filename4.lastIndexOf("\\");
+    if (lastIndex >= 0) {
+      filename4 = filename4.substring(lastIndex + 1);
+    }
+    document.getElementById('filename4').value = filename4;
+  }
+</script>
+
+</body>
+</html>
+
+
