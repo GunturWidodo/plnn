15d7543642f52940623b32b61627199b233e07d6
GunturWidodo <gunturwidodo46@gmail.com>
2019-02-15 11:57:13 +0800

    Add files via upload

--- server/server.php
+++ server/server.php
@@ -1,5 +1,4 @@
 <?php 
-	#session_start();
 
 	//membuat koneksi ke database
 	$db = mysqli_connect('localhost', 'root', '', 'registration');
@@ -14,30 +13,22 @@
 		$tanggal = mysqli_real_escape_string($db, $_POST['tanggal']);
 		$judul = mysqli_real_escape_string($db, $_POST['judul']);
 		$password = mysqli_real_escape_string($db, $_POST['password']);
-
+		$password2 = mysqli_real_escape_string($db, $_POST['password2']);
 		$password = sha1($password);
-		$sql = "INSERT INTO users (nama, nip, unit, jabatan, grade, tanggal, judul, password) VALUES ('$nama', '$nip', '$unit', '$jabatan', '$grade', '$tanggal', '$judul', '$password')";
-		mysqli_query($db, $sql);
+		$password2 = sha1($password2);
 
-		header('location: login.php');	
-	}
+		$sql = "INSERT INTO users (nama, nip, unit, jabatan, grade, tanggal, judul, password) VALUES ('$nama', '$nip', '$unit', '$jabatan', '$grade', '$tanggal','$judul', '$password')";
 
-	if (isset($_POST['simpan'])) {
-		$nip = mysqli_real_escape_string($db, $_POST['nip']);
-		$nama = mysqli_real_escape_string($db, $_POST['nama']);
-		$jabatan = mysqli_real_escape_string($db, $_POST['jabatan']);
-		$unit = mysqli_real_escape_string($db, $_POST['unit']);
-		$kode = mysqli_real_escape_string($db, $_POST['kode']);
-		$judul = mysqli_real_escape_string($db, $_POST['judul']);
-		$pelaksana = mysqli_real_escape_string($db, $_POST['pelaksana']);
-		$nomor = mysqli_real_escape_string($db, $_POST['nomor']);
-		$dari = mysqli_real_escape_string($db, $_POST['dari']);
-		$sampai = mysqli_real_escape_string($db, $_POST['sampai']);
-		$ket = mysqli_real_escape_string($db, $_POST['ket']);
-		
-		$sql = "INSERT INTO sertifikasi (nip, nama, jabatan, unit, kode_sertifikasi, judul_sertifikasi, pelaksana, no_sertifikasi, masa_berlaku, sampai_dengan, keterangan) VALUES ('$nip', '$nama', '$jabatan', '$unit', '$kode', '$judul', '$pelaksana', '$nomor', '$dari', '$sampai', '$ket')";
-		mysqli_query($db, $sql);
+		if($password != $password2){
+			echo '<script language="javascript">';
+			echo 'alert("Konvirmasi password tidak cocok")';
+			echo '</script>';
+			
+		}
+		else if ($password == $password2){
+			mysqli_query($db, $sql);
+			header('location: login.php');	
+		}
 
-		#header('location: ./sertifikasi.php');	
 	}
   ?>
