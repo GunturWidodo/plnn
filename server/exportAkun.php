8d6724649611e6cd47ddbf1ead5f9a79f2b4fe69
GunturWidodo <gunturwidodo46@gmail.com>
2019-02-18 09:55:41 +0800

    Add files via upload

---
+++ server/exportAkun.php
@@ -0,0 +1,30 @@
+<?php  
+  
+$db = mysqli_connect('localhost', 'root', '', 'registration'); 
+  
+$setSql = "SELECT `id`, `nama`, `nip`, `unit`, `jabatan`, `grade`, `tanggal`, `berkas1`, `berkas2`, `berkas3`, `cv`, `talent1`, `talent2`, `talent3` FROM users";  
+$setRec = mysqli_query($db, $setSql);  
+  
+$columnHeader = '';  
+$columnHeader = "NO" . "\t" . "Nama" . "\t" . "NIP" . "\t" . "Unit" . "\t " . "Jabatan" . "\t" . "Grade" . "\t" . "Tanggal grade terakhir" . "\t" . "Berkas 1" . "\t" . "Berkas 2" . "\t" . "Berkas 3" . "\t" . "CV" . "\t" . "Kriteria Talent 1" . "\t" . "Kriteria Talent 2" . "\t" . "Kriteria Talent 3" . "\t";  
+  
+$setData = '';  
+  
+while ($rec = mysqli_fetch_row($setRec)) {  
+    $rowData = '';  
+    foreach ($rec as $value) {  
+        $value = '"' . $value . '"' . "\t";  
+        $rowData .= $value;  
+    }  
+    $setData .= trim($rowData) . "\n";  
+}  
+  
+  
+header("Content-type: application/octet-stream");  
+header("Content-Disposition: attachment; filename=User_Detail_Reoprt.xls");  
+header("Pragma: no-cache");  
+header("Expires: 0");  
+  
+echo ucwords($columnHeader) . "\n" . $setData . "\n";  
+  
+?>
