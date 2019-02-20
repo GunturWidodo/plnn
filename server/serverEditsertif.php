1ce9d357eb4375632f70d9798e8fe9eef5dfe2ae
Ainal Munzir <ainal.munzir@gmail.com>
2019-02-20 08:51:28 +0800

    Update serverEditsertif.php

--- server/serverEditsertif.php
+++ server/serverEditsertif.php
@@ -1,6 +1,21 @@
 <?php 
-$db = mysqli_connect('localhost', 'root', '', 'registration');
-$nip = $_REQUEST['nip'];
+	$db = mysqli_connect('localhost', 'root', '', 'registration');
+	$id = $_REQUEST['nip'];
+
+	$sql = "SELECT * FROM sertifikasi WHERE nip='$id'";
+	$query = mysqli_query($db, $sql);
+	while ($row = mysqli_fetch_array($query)){
+	    $nip = $row['nip'];
+	    $nama = $row['nama'];
+	    $jabatan = $row['jabatan'];
+	    $unit = $row['unit'];
+	    $kode = $row['kode_sertifikasi'];
+	    $judul = $row['judul_sertifikasi'];
+	    $pelaksana = $row['pelaksana'];
+	    $nomor = $row['no_sertifikasi'];
+	    $masa = $row['masa_berlaku'];
+		$sampai = $row['sampai_dengan'];
+	}
 
 	if (isset($_POST['simpan'])) {
 		$nip = mysqli_real_escape_string($db, $_POST['nip']);
@@ -13,14 +28,13 @@ $nip = $_REQUEST['nip'];
 		$nomor = mysqli_real_escape_string($db, $_POST['nomor']);
 		$masa = mysqli_real_escape_string($db, $_POST['dari']);
 		$sampai = mysqli_real_escape_string($db, $_POST['sampai']);
-	}
 
-	$sql = "UPDATE sertifikasi SET nip = '$nip', nama='$nama', jabatan = '$jabatan' , unit = '$unit', kode_sertifikasi = '$kode', judul_sertifikasi = '$judul', pelaksana = '$pelaksana', nomor_sertifikasi = '$nomor', masa_berlaku = '$masa', sampai_dengan = '$sampai' WHERE nip = $nip";			
-
-	if (mysqli_query($db, $sql)){
+		$sql = "UPDATE sertifikasi SET nip='$nip', nama='$nama', jabatan='$jabatan' , unit='$unit', kode_sertifikasi='$kode', judul_sertifikasi='$judul', pelaksana='$pelaksana', no_sertifikasi='$nomor', masa_berlaku='$masa', sampai_dengan='$sampai' WHERE nip='$id'";			
+		mysqli_query($db, $sql);
 		header('editsertifikasi.php');
-	}
-	else{
-		die("error");
+
+		if (!mysqli_query($db, $sql)){
+			die("error");
+		}
 	}
 ?>
