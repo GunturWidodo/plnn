0cc4e57c4bdec388b52f53e5deb2f56037e158d0
GunturWidodo <gunturwidodo46@gmail.com>
2019-02-21 10:50:12 +0800

    Add files via upload

--- server/server.php
+++ server/server.php
@@ -14,12 +14,20 @@
 		$tanggal = mysqli_real_escape_string($db, $_POST['tanggal']);
 		$judul = mysqli_real_escape_string($db, $_POST['judul']);
 		$password = mysqli_real_escape_string($db, $_POST['password']);
-
+		$password2 = mysqli_real_escape_string($db, $_POST['password2']);
 		$password = sha1($password);
+		$password2 = sha1($password2);
+
+		if ($password != $password2) {
+			echo '<script>';
+			echo 'alert("Konfirmasi password tidak benar")';
+			echo '</script>';
+		}
+		else{
 		$sql = "INSERT INTO users (nama, nip, unit, jabatan, grade, tanggal, judul, password) VALUES ('$nama', '$nip', '$unit', '$jabatan', '$grade', '$tanggal', '$judul', '$password')";
 		mysqli_query($db, $sql);
-
 		header('location: login.php');	
+		}
 	}
 
 	if (isset($_POST['simpan'])) {
@@ -50,9 +58,9 @@
 		$d = (int)mysqli_real_escape_string($db, $_POST['nilaiD']);
 		$bobot = (int)mysqli_real_escape_string($db, $_POST['bobot']);
 		$nilai = $lulus+$a+$b+$c+$d+$bobot;
-		$presentasi = ($nilai > 20 ? "-" : "ya");
+		$presentasi = ($nilai > 75 ? "tidak" : "ya");
 
-		$sql = "UPDATE users SET lulus='$lulus', a='$a',b='$b',c='$c',d='$d', bobot='$bobot', nilai='$nilai', presentasi='$presentasi' WHERE nip='$id'";
+		$sql = "UPDATE users SET lulus=$lulus, a='$a',b='$b',c='$c',d='$d', bobot='$bobot', nilai='$nilai', presentasi='$presentasi' WHERE nip='$id'";
 		mysqli_query($db, $sql);
 	}
   ?>
--- server/serverEditsertif.php
+++ server/serverEditsertif.php
@@ -1,8 +1,7 @@
 <?php 
 	$db = mysqli_connect('localhost', 'root', '', 'registration');
-	$id = $_REQUEST['nip'];
-
-	$sql = "SELECT * FROM sertifikasi WHERE nip='$id'";
+	$id = $_REQUEST['id'];
+	$sql = "SELECT * FROM sertifikasi WHERE id = '$id'";
 	$query = mysqli_query($db, $sql);
 	while ($row = mysqli_fetch_array($query)){
 	    $nip = $row['nip'];
@@ -16,7 +15,6 @@
 	    $masa = $row['masa_berlaku'];
 		$sampai = $row['sampai_dengan'];
 	}
-
 	if (isset($_POST['simpan'])) {
 		$nip = mysqli_real_escape_string($db, $_POST['nip']);
 		$nama = mysqli_real_escape_string($db, $_POST['nama']);
@@ -28,13 +26,11 @@
 		$nomor = mysqli_real_escape_string($db, $_POST['nomor']);
 		$masa = mysqli_real_escape_string($db, $_POST['dari']);
 		$sampai = mysqli_real_escape_string($db, $_POST['sampai']);
-
-		$sql = "UPDATE sertifikasi SET nip='$nip', nama='$nama', jabatan='$jabatan' , unit='$unit', kode_sertifikasi='$kode', judul_sertifikasi='$judul', pelaksana='$pelaksana', no_sertifikasi='$nomor', masa_berlaku='$masa', sampai_dengan='$sampai' WHERE nip='$id'";			
+		$sql = "UPDATE sertifikasi SET nip='$nip', nama='$nama', jabatan='$jabatan' , unit='$unit', kode_sertifikasi='$kode', judul_sertifikasi='$judul', pelaksana='$pelaksana', no_sertifikasi='$nomor', masa_berlaku='$masa', sampai_dengan='$sampai' WHERE id='$id'";			
 		mysqli_query($db, $sql);
 		header('editsertifikasi.php');
-
 		if (!mysqli_query($db, $sql)){
-			die("error");
+			die(mysqli_error($db));
 		}
 	}
 ?>
