e822297417f983585fc0be8fc4c272117332fce7
GunturWidodo <gunturwidodo46@gmail.com>
2019-02-22 10:19:13 +0800

    Add files via upload

--- server/donlod.php
+++ server/donlod.php
@@ -3,38 +3,29 @@ session_start();
  $files;
  $user = $_SESSION["login_user"];
 
-   
-    if ($handle = opendir("../upload/berkas/".$user."/")) {
-    while (false !== ($entry = readdir($handle))) {
-      if ($entry != "." && $entry != "..") {
-        $files[] = "../upload/berkas/".$user."/" . $entry;
-      }
-    }
-    closedir($handle);
-  }
-
   # create new zip object
-       $zip = new ZipArchive();
-    
-       # create a temp file &amp; open it
-       $zipname = "test.zip";
-       $zip->open($zipname, ZipArchive::CREATE);
- 
-       # loop through each file
-       foreach($files as $file){
-           # download file
-           $download_file = file_get_contents($file);
- 
-           #add it to the zip
-           $zip->addFromString(basename($file),$download_file);
-       }
- 
-       # close zip
-       
-       $zip->close();
- 
+$zip = new ZipArchive;
+$tmp_file = tempnam('.','');
+    if ($zip->open($tmp_file, ZipArchive::CREATE) === TRUE)
+    {
+        if ($handle = opendir("../upload/berkas/".$user."/"))
+        {
+            // Add all files inside the directory
+            while (false !== ($entry = readdir($handle)))
+            {
+                if ($entry != "." && $entry != "..")
+                {
+                    $zip->addFile("../upload/berkas/".$user."/" . $entry);
+                }
+            }
+            closedir($handle);
+        }
+     
+        $zip->close();
+    }
        # send the file to the browser as a download
-       header('Content-disposition: attachment; filename='.$user.'.zip');
+      header('Content-disposition: attachment; filename='.$user.'.zip');
       header('Content-type: application/zip');
-      readfile($zipname);
+      readfile($tmp_file);
+      unlink($tmp_file);
 ?>
